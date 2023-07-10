24625c7adbdb4470824fc06c0129c8c37184cd2c
abcsumits <76198431+abcsumits@users.noreply.github.com>
2023-07-11 02:34:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const API_KEY="d1845658f92b31c64bd94f06f7188c9c"
 async function showWeather(){
   let city ="goa"
-  
   const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
   const data=await response.json()
   console.log("Weather data:->"+data)
