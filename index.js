1276bec77974705a319fa904a4409e6be650f328
abcsumits <76198431+abcsumits@users.noreply.github.com>
2023-07-11 03:09:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ async function showWeather(){
   let city ="goa"
   const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
   const data=await response.json()
-  console.log("Weather data:->"+data)
+  console.log("Weather data:->"+${data})
   let newPara=document.createElement('p')
   newPara.textContent=`${data?.main?.temp?.toFixed(2)} °C`
   document.body.appendChild(newPara)
