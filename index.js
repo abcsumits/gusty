4cf40f96f3d35ff61d0b4e0fdd4c8ef09b0a07e2
abcsumits <76198431+abcsumits@users.noreply.github.com>
2023-07-14 15:25:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const API_KEY="d1845658f92b31c64bd94f06f7188c9c"
-async function showWeather(){
-  let city ="goa"
-  const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-  const data=await response.json()
-  console.log("Weather data:->"+${data})
-  let newPara=document.createElement('p')
-  newPara.textContent=`${data?.main?.temp?.toFixed(2)} °C`
-  document.body.appendChild(newPara)
+async function fetchWeatherDetails(){
+  try{
+    let city ="goa"
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    const data=await response.json()
+    console.log("Weather data:->"+${data})
+    let newPara=document.createElement('p')
+    newPara.textContent=`${data?.main?.temp?.toFixed(2)} °C`
+    document.body.appendChild(newPara)
+    renderWeatherInfo(data)
+  }
+  catch (err){
+    
+  }
 }
